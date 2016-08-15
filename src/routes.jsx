7978a56490c9fdc72fb9e5f2879075344fe971f3
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2016-08-15 12:01:44 +0200

    routes -- use Plain Routes instead of JSX syntax

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import {Route} from "react-router";
 
-import {App} from "./components/app/App";
-import {TestContainer} from "./components/test/Test";
-import {StudyInbox} from './components/StudyInbox/StudyInbox.jsx';
+import {App} from "src/components/App/App";
+import {StudyInboxContainer} from 'src/components/StudyInbox/StudyInbox.jsx';
 
-const routes = <Route component={App}>
-  <Route path="/test-test" component={TestContainer} />
-  <Route path="/test" component={TestContainer} />
-  <Route path="/inboxes" component={StudyInbox} />
-  <Route path="/" component={TestContainer} />
-</Route>;
+//const routes = <Route component={App}>
+//  <Route path="/test-test" component={TestContainer} />
+//  <Route path="/test" component={TestContainer} />
+//  <Route path="/inboxes" component={StudyInbox} />
+//  <Route path="/" component={TestContainer} />
+//</Route>;
+
+const routes = {
+  path: '/',
+  component: App,
+  childRoutes: [{
+    path: 'inbox',
+    component: StudyInboxContainer
+  }]
+};
 
 export default routes;
