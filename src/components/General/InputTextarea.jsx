9c7e8ada4a646ceb114998af3f48ae07e3093622
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2016-09-25 19:48:28 +0200

    frontend -- InputTextarea -- rc0

--- src/components/General/InputTextarea.jsx
+++ src/components/General/InputTextarea.jsx
@@ -1,3 +1,35 @@
-/**
- * Created by vladmyr on 22.09.2016.
- */
+'use strict';
+
+import React from 'react';
+import InputText from './InputText';
+
+class InputTextarea extends InputText {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const onChangeBound = this.onChange.bind(this);
+    const onBlurBound = this.onBlur.bind(this);
+
+    return <div className="c-input__textarea">
+      {this.props.title
+        ? <label className="c-input__header">{this.props.title}</label>
+        : null
+      }
+      <textarea className="c-input__field"
+        name={this.props.name}
+        placeholder={this.props.placeholder}
+        value={this.state.value}
+        rows={this.props.rows}
+        onChange={onChangeBound}
+        onBlur={onBlurBound}
+      >
+      </textarea>
+    </div>
+  }
+}
+
+InputTextarea.defaultProps.rows = 4;
+
+export default InputTextarea;
